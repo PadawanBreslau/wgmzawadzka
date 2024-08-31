f909910f4bc9b2fced579e162cc7cc2ca5d53e18
Staszek <s.zawadzki@visuality.pl>
2024-08-31 13:08:41 +0200

    WGM Zawadzka start

--- src/App.jsx
+++ src/App.jsx
@@ -29,10 +29,10 @@ const App = () => {
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
+      {false && <Services data={landingPageData.Services} />}
+      {false &&<Gallery data={landingPageData.Gallery} />}
+      {false && <Testimonials data={landingPageData.Testimonials} />}
+      {false &&<Team data={landingPageData.Team} />}
       <Contact data={landingPageData.Contact} />
     </div>
   );
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -42,10 +42,9 @@ export const Contact = (props) => {
           <div className="col-md-8">
             <div className="row">
               <div className="section-title">
-                <h2>Get In Touch</h2>
+                <h2>Napisz do mnie</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  Proszę wypełnić formularz poniżej, odpowiem najszybciej jak to możliwe
                 </p>
               </div>
               <form name="sentMessage" validate onSubmit={handleSubmit}>
@@ -57,7 +56,7 @@ export const Contact = (props) => {
                         id="name"
                         name="name"
                         className="form-control"
-                        placeholder="Name"
+                        placeholder="Imię"
                         required
                         onChange={handleChange}
                       />
@@ -85,7 +84,7 @@ export const Contact = (props) => {
                     id="message"
                     className="form-control"
                     rows="4"
-                    placeholder="Message"
+                    placeholder="Wiadomość"
                     required
                     onChange={handleChange}
                   ></textarea>
@@ -93,17 +92,17 @@ export const Contact = (props) => {
                 </div>
                 <div id="success"></div>
                 <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
+                  Wyślij
                 </button>
               </form>
             </div>
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
             <div className="contact-item">
-              <h3>Contact Info</h3>
+              <h3>Dane kontaktowe</h3>
               <p>
                 <span>
-                  <i className="fa fa-map-marker"></i> Address
+                  <i className="fa fa-map-marker"></i> Adres
                 </span>
                 {props.data ? props.data.address : "loading"}
               </p>
@@ -111,7 +110,7 @@ export const Contact = (props) => {
             <div className="contact-item">
               <p>
                 <span>
-                  <i className="fa fa-phone"></i> Phone
+                  <i className="fa fa-phone"></i> Nr tel.
                 </span>{" "}
                 {props.data ? props.data.phone : "loading"}
               </p>
@@ -134,16 +133,6 @@ export const Contact = (props) => {
                       <i className="fa fa-facebook"></i>
                     </a>
                   </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
                 </ul>
               </div>
             </div>
@@ -153,10 +142,7 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
+             Warszawa 2024, 3Mountains
           </p>
         </div>
       </div>
